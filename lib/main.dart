08c60bcbd876e7cdc58ca74693c7eda8426a32c9
syamsul <80880752+syasul@users.noreply.github.com>
2021-09-19 18:28:15 +0700

    add

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:sign_in/Screens/Signup.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignUp(),
+    );
+  }
+}
